d3d5dc533247f0768b6e8cfadff2bef46a1a1468
Stathis Gennatas <gennatas@gmail.com>
2026-03-28 02:47:14 -0700

    refactor test_fmt to include output_type for ansi and plain formats

--- tests/testthat/test_fmt.R
+++ tests/testthat/test_fmt.R
@@ -3,9 +3,16 @@
 # 2026- EDG rtemis.org
 
 # %% fmt
-test_that("fmt works", {
+test_that("fmt works with ansi output", {
   expect_equal(
-    fmt("Hello", col = "#008080", bold = TRUE),
+    fmt("Hello", col = "#008080", bold = TRUE, output_type = "ansi"),
     "\033[1;38;2;0;128;128mHello\033[0m"
+  )
+})
+
+test_that("fmt works with plain output", {
+  expect_equal(
+    fmt("Hello", col = "#008080", bold = TRUE, output_type = "plain"),
+    "Hello"
   )
 })
